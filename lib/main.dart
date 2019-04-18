b65d9b218a08cb3bf28827b92721d06c1a888eb8
angusfiedler <angus.fiedler@gmail.com>
2019-04-18 12:45:08 -0600

    Began implementation of json data retrieval

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'newItem.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+//Class for json data
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
 
 void main() => runApp(MyApp());
 
@@ -26,11 +48,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String lat = "Latitude: NULL", longitude = "Longitude: NULL";
+  String lat = "Latitude: NULL",
+      longitude = "Longitude: NULL",
+      speed = "Speed: ";
   Location _location = new Location();
 
+  //fetches data from json and places into POST
+  Future<Post> fetchPost() async {
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/posts/1');
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
 
-  void refreshData() async{
+  //refreshes the users current location
+  void refreshData() async {
     LocationData userLocation;
     userLocation = await _location.getLocation();
     print(userLocation.latitude);
@@ -39,6 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       lat = "Latitude: " + userLocation.latitude.toString();
       longitude = "Longitude: " + userLocation.longitude.toString();
+      speed = "Speed: " + userLocation.speed.toString();
+      fetchPost();
     });
   }
 
@@ -65,11 +104,32 @@ class _MyHomePageState extends State<MyHomePage> {
                   recoTextWidget(),
                 ],
               ),
-            )
+            )),
+            FlatButton(
+              child: Text(
+                "Refresh Data",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                refreshData();
+              },
+              color: Colors.red,
             ),
-            FlatButton(child: Text("Refresh Data", style: TextStyle(color: Colors.white),),onPressed: () {refreshData();},color: Colors.red,),
             Text(lat),
             Text(longitude),
+            Text(speed),
+            FutureBuilder<Post>(
+              future: fetchPost(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.body);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                // By default, show a loading spinner
+                return CircularProgressIndicator();
+              },
+            ),
           ],
         ),
       ),
@@ -86,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget recoTextWidget(){
+  Widget recoTextWidget() {
     return Column(
       children: <Widget>[
         Text("1. INSERT HERE"),
@@ -98,7 +158,6 @@ class _MyHomePageState extends State<MyHomePage> {
         Text("4. INSERT HERE"),
         Divider(),
         Text("5. INSERT HERE"),
-
       ],
     );
   }
